4819ed5b5742b8cd7eba8328ef851fe8bad6e354
Fernanda Avendaño Jara <fernanda.avendano@sansano.usm.cl>
2023-09-01 15:51:53 -0400

    Avance de encriptado y agregar contraseña

--- codigo/contraseña.py
+++ codigo/contraseña.py
@@ -1,14 +1,40 @@
+import hashlib
+
 class Usuario:
     #constructor
     def __init__(self, nombre_usuario, email_usuario):
-        self.nombre_usuario = nombre_usuario
         self.email_usuario= email_usuario
         self.pass_list = {}
+        self.cargar_datos()
+        
+    def cargar_datos(self):
+        try:
+            with open('contraseñas.txt', 'r') as archivo:
+                texto = archivo.readlines()
+                for linea in texto:
+                    if linea.startswith(f'Usuario: {self.email_usuario}'):
+                        contenido = linea.strip().split(", ")
+                        for partes in contenido:
+                            if "Plataforma: " in partes:
+                                plataforma = partes.split(": ")[1]
+                            elif "Contraseña: " in partes:
+                                contrasena = partes.split(": ")[1]
+                        self.pass_list[plataforma] = contrasena
+        except FileNotFoundError:
+            pass
+        
+    def encriptado_contrasena(self, contrasena):
+        encriptado=hashlib.sha256(contrasena.encode())
+        return encriptado.hexdigest()
     
     def agregar_constrasena(self, buffer, contrasena, email):
-        if self.email_usuario == email :
-            self.pass_list[buffer] = contrasena
-    
+            self.pass_list[buffer] = self.encriptado_contrasena(contrasena)
+            self.guardar_contrasena(buffer, self.pass_list[buffer]) 
+
+    def guardar_contrasena(self, buffer, contrasena_encriptada):
+        with open('contraseñas.txt', 'a') as file:
+            file.write(f'Usuario: {self.email_usuario}, Plataforma: {buffer}, Contraseña: {contrasena_encriptada}\n')
+
     def eliminar_contrasena(self, email):
         if self.email_usuario == email :
             print("¿Qué constraseña deseas eliminar?")
@@ -25,8 +51,6 @@ class Usuario:
                 
                 else:
                     del self.pass_list[remover]
-            
-            
             except:
                 print("Opción no válida")
 
@@ -110,4 +134,4 @@ try:
         usuario_ej.eliminar_contrasena(correo)
 
 except ValueError:
-   exit('Opción no valida')
+   exit('Opción no valida') 
